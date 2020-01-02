915dc53b6008efa2e1aadc2938af65ad6ff1a29b
Oren <horenh9>
2020-01-02 23:42:21 +0200

    UpTree

--- UpTree.h
+++ UpTree.h
@@ -5,18 +5,18 @@ template<class T>
 class UpVertex {
 public:
     int key;
-    T *parent;
     int size;
+    T *parent;
+    T *data;
 };
 
 template<class T>
 class UpTree {
 public:
-    UpVertex<T> *head;
-    UpVertex<T> *parents;
+    UpVertex<T> **parents;
     int size;
 
-    UpTree(int size) : head(nullptr), size(size) {
+    UpTree(int size) : size(size) {
         for (int i = 1; i < size + 1; ++i) {
             UpVertex<T> vertex = new UpVertex<T>;
             parents[i] = new UpVertex<T>(vertex);
@@ -26,20 +26,25 @@ public:
     T Find(int a) {
         if (a > size || a < 1)
             return nullptr;
-        while (parents[a] != head) {
-            int temp = a;
-            a = parents[a].key;
-            parents[temp] = head;
+        int temp1 = a;
+        while (parents[a]->key != a) {
+            int temp2 = a;
+            a = parents[a]->key;
+        }
+        while (parents[temp1]->key != temp1) {
+            int temp2 = temp1;
+            temp1 = parents[temp1]->key;
+            parents[temp2]->key = a;
         }
     }
 
     T Union(int a, int b) {
         if (a > size || b > size || a < 1 || b < 1)
             return nullptr;
-        if (parents[a].size > parents[b].size) {
+        if (parents[a]->size > parents[b]->size) {
             parents[b] = parents[a];
-            parents[a].size += parents[b].size;
-            parents[b].parent = parents[a].parent;
+            parents[a]->size += parents[b]->size;
+            parents[b]->parent = parents[a]->parent;
             return Find(b);
         } else
             return Union(b, a);
